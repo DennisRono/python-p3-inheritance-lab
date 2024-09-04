b2c7d7d247b610a86989593fb627fa74f32a90e8
Dennis Kibet <dennisrkibet@gmail.com>
2024-09-04 11:31:07 +0300

    passed the unit tests

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,12 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        # Initialize the base User class with first and last names
+        super().__init__(first_name, last_name)
+        # Initialize the knowledge attribute as an empty list
+        self.knowledge = []
+
+    def learn(self, info):
+        # Add the information to the knowledge list
+        self.knowledge.append(info)
